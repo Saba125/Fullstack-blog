e3701fde3bc6dbf7f210bf107335da2de0299947
Saba125 <jiadzeb@gmail.com>
2023-12-19 16:43:56 +0400

    login-page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,10 +1,28 @@
-
+import styles from "@/pages/LoginPage.module.css";
 const LoginPage = () => {
-    return ( 
-        <div>
-            
+  return (
+    <div className="container">
+        <div className={styles.wrapper}>
+            <h3 className={styles.title}>Sign in here   </h3>
+            <div className={styles.box}>
+                <form className={styles.form}>
+                    <div>
+                        <label>Username</label>
+                        <input type="text" placeholder="Enter name here" />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter password here" />
+                    </div>
+                    <div className={styles.button}>
+                        <button>Submit</button>
+                    </div>
+                </form>
+                
+            </div>
         </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default LoginPage;
